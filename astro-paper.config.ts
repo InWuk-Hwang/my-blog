0d16ee9a59c10b644d0e288289dc44ea28d3b795
InWuk-Hwang <muzigae29@gmail.com>
2026-09-15 22:41:42 +0900

    fix: remove url property when editPost is disabled

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -9,7 +9,7 @@ export default defineAstroPaperConfig({
     profile: "https://thepastpatch.com",
     ogImage: "default-og.jpg",
     lang: "en-US",
-    timezone: "America/New_York",
+    timezone: "Asia/Seoul",
     dir: "ltr",
   },
   posts: {
@@ -23,23 +23,18 @@ export default defineAstroPaperConfig({
     showArchives: true,
     showBackButton: true,
     editPost: {
-      enabled: false, // satnaing 원본 레포 링크가 노출되지 않도록 초기에는 false 권장
-      url: "https://github.com/InWuk-Hwang/my-blog/edit/main/",
+      enabled: false,
     },
     search: "pagefind",
   },
   socials: [
-    { name: "github",   url: "https://github.com/satnaing/astro-paper" },
-    { name: "x",        url: "https://x.com/username" },
-    { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
-    { name: "mail",     url: "mailto:yourmail@gmail.com" },
+    { name: "github",   url: "https://github.com/InWuk-Hwang/my-blog" },
+    { name: "mail",     url: "mailto:Muzigae29@gmail.com" },
   ],
   shareLinks: [
-    { name: "whatsapp", url: "https://wa.me/?text=" },
-    { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
     { name: "x",        url: "https://x.com/intent/post?url=" },
+    { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
     { name: "telegram", url: "https://t.me/share/url?url=" },
-    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
     { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
   ],
 });
